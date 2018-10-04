1c52256b8da283ac86c4209bf0911a51116412da
strifeex <strifeex@gamil.com>
2018-10-04 22:50:01 +0700

    remove blank lines

--- public/category.php
+++ public/category.php
@@ -1,20 +1,16 @@
-<?php require_once("../resources/config.php"); ?>
-
-<?php include(TEMPLATE_FRONT . DS . "html_header.php") ?>
-
+<?php
+require_once "../resources/config.php";
+include TEMPLATE_FRONT . DS . "html_header.php"
+?>
     <!-- Page Content -->
     <div class="container">
-
         <!-- Jumbotron Header -->
         <header class="jumbotron hero-spacer">
             <h1>A Warm Welcome!</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, ipsam, eligendi, in quo sunt possimus non incidunt odit vero aliquid similique quaerat nam nobis illo aspernatur vitae fugiat numquam repellat.</p>
-            
             </p>
         </header>
-
         <hr>
-
         <!-- Title -->
         <div class="row">
             <div class="col-lg-12">
@@ -22,20 +18,11 @@
             </div>
         </div>
         <!-- /.row -->
-
         <!-- Page Features -->
         <div class="row text-center">
-
-         <?php get_products_in_cat_page(); ?>
-
-
+         <?php get_products_in_cat_page();?>
         </div>
         <!-- /.row -->
-
-
-
     </div>
     <!-- /.container -->
-
-
-<?php include(TEMPLATE_FRONT . DS . "html_footer.php") ?>
+<?php include TEMPLATE_FRONT . DS . "html_footer.php"?>
--- resources/config.php
+++ resources/config.php
@@ -1,38 +1,16 @@
 <?php
-
-
 ob_start();
-
 session_start();
-// session_destroy();
 
 defined("DS") ? null : define("DS", DIRECTORY_SEPARATOR);
-
-
 defined("TEMPLATE_FRONT") ? null : define("TEMPLATE_FRONT", __DIR__ . DS . "templates/front");
-
 defined("TEMPLATE_BACK") ? null : define("TEMPLATE_BACK", __DIR__ . DS . "templates/back");
-
-
 defined("UPLOAD_DIRECTORY") ? null : define("UPLOAD_DIRECTORY", "../resources/uploads");
-
-
 defined("DB_HOST") ? null : define("DB_HOST", "188.166.227.90");
-
-defined("DB_USER") ? null : define("DB_USER","connection");
-
-
+defined("DB_USER") ? null : define("DB_USER", "connection");
 defined("DB_PASS") ? null : define("DB_PASS", "123456");
+defined("DB_NAME") ? null : define("DB_NAME", "test-ecom");
+$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
-defined("DB_NAME") ? null : define("DB_NAME",  "test-ecom");
-
-
-
-$connection = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
-
-require_once("functions.php");
-require_once("cart.php");
-
-
-
- ?>
+require_once "functions.php";
+require_once "cart.php";
